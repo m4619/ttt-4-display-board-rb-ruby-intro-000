4cda6164849f6769d665264e95d733311d304951
determined-pipe-6216 <marreola4619@gmail.com>
2020-10-22 18:04:36 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(x)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+print display_board(board[5])
